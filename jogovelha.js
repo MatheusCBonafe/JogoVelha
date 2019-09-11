153cfc0bb00087a6ede3fb5b2d8da3409736202d
MatheusCBonafe <warpmatheus@gmail.com>
2019-09-11 03:51:47 -0300

    CSS e Ajustes

--- jogovelha.js
+++ jogovelha.js
@@ -1,7 +1,4 @@
-// alert("test");
-var buttonAux = "O";
-
-
+var buttonAux = "X";
 
 function resetBoard() {
   var pos1 = document.getElementById("1");
@@ -38,53 +35,40 @@ function resetBoard() {
   document.getElementById("overlay").style.visibility = "hidden";
 }
 
-function disableBoard() {
-  var pos1 = document.getElementById("1");
-  var pos2 = document.getElementById("2");
-  var pos3 = document.getElementById("3");
-  var pos4 = document.getElementById("4");
-  var pos5 = document.getElementById("5");
-  var pos6 = document.getElementById("6");
-  var pos7 = document.getElementById("7");
-  var pos8 = document.getElementById("8");
-  var pos9 = document.getElementById("9");
-
-  pos1.disabled = true;
-  pos2.disabled = true;
-  pos3.disabled = true;
-  pos4.disabled = true;
-  pos5.disabled = true;
-  pos6.disabled = true;
-  pos7.disabled = true;
-  pos8.disabled = true;
-  pos9.disabled = true;
-}
-
 function winnerMessage(winner) {
-  disableBoard();
 
-  popuptext = document.getElementById("text");
-  popuptext.innerHTML = winner + " venceu.";
+  switch (winner) {
+    case "O":
+      winner = "Jogador 1";
+      popuptext = document.getElementById("text");
+      popuptext.innerHTML = winner + " venceu.";
 
-  var pop = document.getElementById("popup");
-  var overlay = document.getElementById("overlay");
-  pop.style.visibility = "visible";
-  overlay.style.visibility ="visible";
+      var pop = document.getElementById("popup");
+      var overlay = document.getElementById("overlay");
+      pop.style.visibility = "visible";
+      overlay.style.visibility ="visible";
+      break;
+    case "X":
+      winner = "Jogador 2";
+      popuptext = document.getElementById("text");
+      popuptext.innerHTML = winner + " venceu.";
+
+      var pop = document.getElementById("popup");
+      var overlay = document.getElementById("overlay");
+      pop.style.visibility = "visible";
+      overlay.style.visibility ="visible";
+      break;
+    case "draw":
+      popuptext = document.getElementById("text");
+      popuptext.innerHTML = "Empate";
+
+      var pop = document.getElementById("popup");
+      var overlay = document.getElementById("overlay");
+      pop.style.visibility = "visible";
+      overlay.style.visibility ="visible";
+      break;
+    }
 }
-
-function drawMessage() {
-  disableBoard();
-
-  popuptext = document.getElementById("text");
-  popuptext.innerHTML = "Empate.";
-
-  var pop = document.getElementById("popup");
-  var overlay = document.getElementById("overlay");
-  pop.style.visiblity = "visible";
-  overlay.style.visibility = "visible";
-
-}
-
 
 function winCondition() {
   var pos1 = document.getElementById("1").value;
@@ -96,34 +80,35 @@ function winCondition() {
   var pos7 = document.getElementById("7").value;
   var pos8 = document.getElementById("8").value;
   var pos9 = document.getElementById("9").value;
+  var draw = "draw";
 
-  if (((pos1 == "X") || (pos1 == "O")) && ((pos1 == pos2) && (pos2 == pos3)))
+  if ((pos1 == "X" || pos1 == "O") && (pos1 == pos2 && pos2 == pos3))
     winnerMessage(pos1);
 
-  else if (((pos1 == "X") || (pos1 == "O")) && ((pos1 == pos4) && (pos4 == pos7)))
+  else if ((pos1 == "X" || pos1 == "O") && (pos1 == pos4 && pos4 == pos7))
     winnerMessage(pos1);
 
-  else if (((pos1 == "X") || (pos1 == "O")) && ((pos1 == pos5) && (pos5 == pos9)))
+  else if ((pos1 == "X" || pos1 == "O") && (pos1 == pos5 && pos5 == pos9))
     winnerMessage(pos1);
 
   else if ((pos2 == "X" || pos2 == "O") && (pos2 == pos5 && pos5 == pos8))
     winnerMessage(pos2);
 
-  else if (((pos4 == "X") || (pos4 == "O")) && ((pos4 == pos5) && (pos5 == pos6)))
+  else if ((pos4 == "X" || pos4 == "O") && (pos4 == pos5 && pos5 == pos6))
     winnerMessage(pos4);
 
-  else if (((pos7 == "X") || (pos7 == "O")) && ((pos7 == pos5) && (pos5 == pos3)))
+  else if ((pos7 == "X" || pos7 == "O") && (pos7 == pos5 && pos5 == pos3))
     winnerMessage(pos7);
 
-  else if (((pos9 == "X") || (pos9 == "O")) && ((pos9 == pos8) && (pos8 == pos7)))
+  else if ((pos9 == "X" || pos9 == "O") && (pos9 == pos8 && pos8 == pos7))
     winnerMessage(pos9);
 
-  else if (((pos9 == "X") || (pos9 == "O")) && ((pos9 == pos6) && (pos6 == pos3)))
+  else if ((pos9 == "X" || pos9 == "O") && (pos9 == pos6 && pos6 == pos3))
     winnerMessage(pos9);
 
-  //else if  (pos1.value != "" && pos2.value != "" && pos3.value != "" && pos4.value != "" && pos5.value != "" &&
-   //pos6.value != "" && pos7.value != "" && pos8.value != "" && pos9.value != "")
-    //drawMessage();
+  else if ((pos1 !== "" && pos2 !== "" && pos3 !== "" && pos4 !== "" && pos5 !== "" && pos6 !== ""
+    && pos7 !== "" && pos8 !== "" && pos9 !== ""))
+    winnerMessage(draw);
 }
 
 function disablePosition(x, buttonAux) {
@@ -180,10 +165,12 @@ function disablePosition(x, buttonAux) {
 
 function buttonPress(button) {
   if (buttonAux == "X") {
+    document.getElementById("jogador").innerHTML = "Jogador 2";
     buttonAux = "O";
     disablePosition(button, buttonAux);
   }
   else if (buttonAux == "O") {
+    document.getElementById("jogador").innerHTML = "Jogador 1";
     buttonAux = "X";
     disablePosition(button, buttonAux);
   }
